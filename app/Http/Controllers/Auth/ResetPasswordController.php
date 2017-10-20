1e932a345134919dc1e93fa261f02249d18daa3e
Marco Paco <marco.paco@personiv.com>
2017-10-21 04:52:57 +0800

    Reset password for api

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -5,6 +5,12 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\Events\PasswordReset;
+
+
 class ResetPasswordController extends Controller
 {
     /*
@@ -35,6 +41,38 @@ class ResetPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+    /**
+     * Get the password reset validation rules.
+     *
+     * @return array
+     */
+    protected function rules()
+    {
+        return [
+            'token' => 'required',
+            'email' => 'required|email|exists:password_resets',
+            'password' => 'required|confirmed|min:6',
+        ];
+    }
+
+    /**
+     * Reset the given user's password.
+     *
+     * @param  \Illuminate\Contracts\Auth\CanResetPassword  $user
+     * @param  string  $password
+     * @return void
+     */
+    protected function resetPassword($user, $password)
+    {
+        $user->password = Hash::make($password);
+
+        $user->setRememberToken(Str::random(60));
+
+        $user->save();
+
+        event(new PasswordReset($user));
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Http\Request;
 */
 
 Route::post('password/reset', 'Auth\ForgotPasswordController@sendResetLinkEmail');
-Route::post('password/reset/{token}', 'Auth\ResetsPasswordsController@reset');
+Route::post('password/reset/{token}', 'Auth\ResetPasswordController@reset');
 
 Route::group(['prefix' => 'user'], function () {
     Route::post('auth', 'UserController@auth');
